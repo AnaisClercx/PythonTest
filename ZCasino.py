9e3f496a075cf31a195033e8b14df5b99b74b9bc
AnaisClercx <dubitativus@hotmail.fr>
2016-12-12 15:07:32 +0000

    with english comments 1

--- ZCasino.py
+++ ZCasino.py
@@ -2,17 +2,17 @@
 from random import randrange
 from math import ceil
 argentDepart=100
-while (argentDepart >0):
+while (argentDepart >0): 
 	print (argentDepart,"$")
-	mise = input ("how much do you want play?")
-	mise = int (mise)
-	caseChoisi = input("on which box?")
-	caseChoisi = int(caseChoisi)
-	x= ceil(mise/2)
-	nbAlleatoire = randrange (50)
-	caseRouge=nbAlleatoire%2
-	print ("the nb winner is :", nbAlleatoire)
-	if (caseChoisi==nbAlleatoire):
+	mise = input ("how much do you want play?") # recovert the variable mise
+	mise = int (mise) #transform mise in int
+	caseChoisi = input("on which box?") # recovert the variable caseChoisi
+	caseChoisi = int(caseChoisi) # transform caseChoisi in int
+	x= ceil(mise/2) #Rounded to the nearest number
+	nbAlleatoire = randrange (50) #random 50
+	caseRouge=nbAlleatoire%2 # determine the color
+	print ("the nb winner is :", nbAlleatoire) # diplay random nb
+	if (caseChoisi==nbAlleatoire): 
 		argentDepart= argentDepart + mise*3
 		print("well done! You win", mise*3)
 	if (caseChoisi%2 == caseRouge):
